a2c4b1c2af0ed77c369016a965584c10c876bbf3
Vaagish hambiria <vaagish@Vaagishs-MacBook-Air.local>
2022-12-17 14:55:48 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,122 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.containerOuterBox}>
       <StatusBar style="auto" />
+
+    <View style={styles.containerBox}>
+
+      <View style={[styles.container2, styles.shadowProp]}>
+      <View style={styles.icon}>
+            <Ionicons name="md-checkmark-circle" size={32} color="green" />
+          </View>
+
+          <View style={{width: '90%', alignItems: 'center'}}> 
+            <Text style={styles.myTextAb}>Record Purchase</Text>
+          </View>
+      </View>
+
+    
+      <View style={[styles.container1, {paddingBottom: 10}]}>
+          <View style={styles.icon}>
+            <Ionicons name="md-checkmark-circle" size={32} color="green" />
+          </View>
+
+          <View style={{width: '90%', alignItems: 'center', paddingLeft: 20}}> 
+            <Text style={styles.myText}>Rewards & Benefits</Text>
+          </View>
+      </View>
+
+      <View style={styles.container1}>
+          <View style={styles.icon}>
+            <Ionicons name="md-checkmark-circle" size={32} color="green" />
+          </View>
+
+          <View style={{width: '90%', alignItems: 'center', paddingLeft: 20}}> 
+            <Text style={styles.myText}>Products & Schemes</Text>
+          </View>
+      </View>
+
+      <View style={[styles.container1, {paddingBottom: 10}]}>
+          <View style={styles.icon}>
+            <Ionicons name="md-checkmark-circle" size={32} color="green" />
+          </View>
+
+          <View style={{width: '85%', alignItems: 'center'}}> 
+            <Text style={styles.myText}>Help & Support</Text>
+          </View>
+      </View>
+
+      <View style={styles.container1}>
+          <View style={styles.icon}>
+            <Ionicons name="md-checkmark-circle" size={32} color="green" />
+          </View>
+
+          <View style={{width: '85%', alignItems: 'center'}}> 
+            <Text style={styles.myText}>My Business</Text>
+          </View>
+      </View>
+
+      </View>
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOuterBox: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  containerBox: {
+    // borderWidth: 1,
+    width: '100%',
+    height: Dimensions.get('screen').height/2,
+    flexWrap: 'wrap'
+  }, 
+  container1: {
+    borderWidth: 1,
+    width: '50%',
+    height: '50%',
+    justifyContent: 'center', 
+    alignItems: 'center',
+    backgroundColor: '#eeeeee',
+    borderColor: 'gray'
+  },
+  container2: {
+    width: 140,
+    height: 140,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    position: 'absolute',
+    zIndex: 1,
+    top: '33%',
+    left: '31%',
+    borderRadius: 100
+  },
+  myText: {
+    marginTop: 5,
+    fontSize: 20,
+    fontWeight: '700'
+  },
+  myTextAb: {
+    fontSize: 18,
+    fontWeight: '700',
+    textAlign: 'center'
+  },
+  shadowProp: {
+    shadowColor: '#171717',
+    shadowOffset: {width: -50, height: 40},
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
   },
 });
